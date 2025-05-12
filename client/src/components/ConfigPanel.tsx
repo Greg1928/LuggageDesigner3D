d86f616740df73a31ad9a86b3a64d5e6bcc7c339
gregoriosortino <12416839-gregoriosortino@users.noreply.replit.com>
2025-05-12 09:45:04 +0000

    Restored to 'cc7c04a2c051419c3e44ad83511a271f11597417'
    Replit-Restored-To: cc7c04a2c051419c3e44ad83511a271f11597417

--- client/src/components/ConfigPanel.tsx
+++ client/src/components/ConfigPanel.tsx
@@ -144,7 +144,7 @@ export function ConfigPanel() {
                 onClick={handleResetConfig}
               >
                 <TimerReset className="h-4 w-4 mr-2" />
-                Reset
+                TimerReset
               </Button>
               <Button 
                 variant="outline" 
